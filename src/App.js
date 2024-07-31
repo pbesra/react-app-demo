0ebc50ecc10853cc4f0881eb7dbe9cde3d556829
Besra <I43909@verisk.com>
2024-07-31 10:27:03 +0530

    import root reducer

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import "./App.css";
 import Main from "./components/Main/Main";
 import { Provider } from "react-redux";
 import store from "./reduxStore/store";
+import rootReducer from "./reduxStore/rootReducer";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <div>
-          <Main />
-        </div>
-      </div>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<div className="App">
+				<div>
+					<Main />
+				</div>
+			</div>
+		</Provider>
+	);
 }
 
 export default App;
